8eade10da3a4cb1edd7a71f077405c66b37fd066
Charlie Stoner <c.stoner@ufl.edu>
2025-10-14 13:57:34 -0400

    frontend: basic configuration + created type definitions, api client, and functions for authentification

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['localhost', 'djangobackend.com'],  // Add backend domain here
+    formats: ['image/webp', 'image/avif'],
+},
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',  // Default to local backend if not set
+  },
 };
 
 export default nextConfig;
